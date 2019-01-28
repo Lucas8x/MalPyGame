c8d5148a04d449bcabcee511d31b7297918d0d5f
Lucas Emanuel <lucas.poring99@gmail.com>
2019-01-28 10:53:54 -0300

    Update malpygame.py

--- malpygame.py
+++ malpygame.py
@@ -1,6 +1,7 @@
 from lxml import html
 import requests
 # import myanimelist.session
+import re
 import os
 import time
 from selenium import webdriver
@@ -51,29 +52,32 @@ def check():
   driver.get(thread)
   driver.refresh()
   tree = html.fromstring(driver.page_source)
-  users = tree.xpath('//*[@style="padding-bottom: 2px;"]/a/strong/text()')
-  # post = tree.xpath('//*[@class="clearfix"]/text()')
+  #users = tree.xpath('//*[@style="padding-bottom: 2px;"]/a/strong/text()')
+  users = tree.xpath('//*[contains(@id,"messageuser")]/a/strong/text()')
+  #post = tree.xpath('//*[@class="clearfix"]/text()')
   post = tree.xpath('//*[contains(@id,"message")]/text()')
 
-  #print("\nUsers: ", *users)
-  #print("\nLast User: ", users[-1])
-  print("Posts: ", *post)
+  print("Users: ", *users)
+  print("Last User: ", str(users[-1]))
+  print("\nPosts: ", *post)
 
-  # filt = [ x for x in post if x.isdigit() ]
-  filt = ''.join(filter(lambda x: x in '0123456789', str(post)))
-  filt = [filt[i:i + 3] for i in range(0, len(filt), 3)]
+  #filt = [ x for x in post if x.isdigit() ]
+  #filt = ''.join(filter(lambda x: x in '0123456789', str(post)))
+  #filt = [filt[i:i + 3] for i in range(0, len(filt), 3)]
+  filt = [re.sub('[^0-9]','', x) for x in post]
 
   print("Filter:", *filt)
   print("Last Post: ", filt[-1])
-  calc = filt[-1] = int(filt[-1])-1
+  try: calc = int(filt[-1])-1
+  except TypeError: return
   print("Calculation:", calc)
 
-  lastuser = users[-1] = str(users[-1])
+  lastuser = str(users[-1])
   print("\nChecking if last user is you...")
   if (lastuser == ""):
       print("Yes, it's you", lastuser, "nothing will happen")
   else:
-      print("Its not, trying post...\n")
+      print("Itsn't, trying post...\n")
       driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
       driver.find_element_by_id('showQuickReply').click()
       driver.find_element_by_id("messageText").send_keys(calc)
